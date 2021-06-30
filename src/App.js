5d8b23e6239795a13beaa5bdd28a078fde10da94
Oleg-Demin <79312793+Oleg-Demin@users.noreply.github.com>
2021-06-30 03:03:21 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -5,70 +5,66 @@ import dataJson from './data/data.json'
 
 class App extends Component {
   
-  state = {
-    data: dataJson,
-    direction: 1,
-    indexSort: ''
-  }
-
-  sortData = (index) => {
-    const copyData = this.state.data.concat();
-    // const copyDirection = this.state.direction;
-    // const nameField = this.state.nameField;
-
-    if (index === '#'){
-      this.setState({
-        data: dataJson,
-        direction: 1,
-        indexSort: index
-      })
-      return;
-    }
-
-    if (index === this.state.indexSort){
-      copyData.sort(
-        (a, b) => {
-          if (a[index] > b[index]){
-            return this.state.direction;
-          } else {
-            return -this.state.direction;
-          }
-        }
-      )
-      this.setState({
-        data: copyData,
-        direction: -this.state.direction,
-        indexSort: index
-      })
-    } else {
-      copyData.sort(
-        (a, b) => {return a[index] > b[index] ? 1 : -1}
-      )
-      this.setState({
-        data: copyData,
-        direction: -1,
-        indexSort: index
-      })
-    }
-  }
-
-  // Фильтрация значений на основании того, что было записано в input    
-  filteredData = (indexFiltered, searchFiltered) => {
-    return this.state.data.filter(
-        item => String(item[indexFiltered]).toLowerCase().includes(searchFiltered.toLowerCase())
-    )
-  }
-  
   render() {
+
+    // Необходимо чтобы индексы были уникальными (поэтому нет необходимости в id)
+    const tableColumns1 = [
+      { index: "id", name: "Id"},
+      { index: "surname", name: "Фамилия"},
+      { index: "name", name: "Имя"},
+      { index: "patronymic", name: "Отчество"},
+      { index: "phone", name: "Телефон"},
+      { index: "date", name: "Дата"},
+    ]
+
+    const tableColumns2 = [
+      // { index: "id", name: "Id"},
+      { index: "name", name: "Имя"},
+      { index: "surname", name: "Фамилия"},
+      // { index: "patronymic", name: "Отчество"},
+      // { index: "phone", name: "Телефон"},
+      // { index: "date", name: "Дата"},
+    ]
+
+    const tableColumns3 = [
+      // { index: "id", name: "Id"},
+      // { index: "surname", name: "Фамилия"},
+      // { index: "name", name: "Имя"},
+      // { index: "patronymic", name: "Отчество"},
+      { index: "phone", name: "Телефон"},
+      { index: "date", name: "Дата"},
+    ]
+
     return (
       <div className="container-xxl">
-          <Table
-            // data={this.state.data}
-            sortData={this.sortData}
-            filteredData={this.filteredData}
-            direction={this.state.direction}
-            indexSort={this.state.indexSort}
-          />
+
+        <div className="row">
+          <div className="col">
+            <Table
+              data = {dataJson}
+              columns = {tableColumns1}
+              theadColor = "#e09eff"
+            />
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-4">
+            <Table
+              data = {dataJson}
+              columns = {tableColumns2}
+              theadColor ="#9effc2"
+            />
+          </div>
+          <div className="col-8">
+            <Table
+              data = {dataJson}
+              columns = {tableColumns3}
+              theadColor = "#ffe09e"
+            />
+          </div>
+        </div>
+
       </div>
     );
   }
--- src/table/table.js
+++ src/table/table.js
@@ -1,147 +1,197 @@
-import caretDown from '../assets/caret-down-fill.svg';
-import caretUp from '../assets/caret-up-fill.svg';
-import caretLeft from '../assets/caret-left.svg';
-import funnel from '../assets/funnel-fill.svg';
 import React, { Component } from 'react';
-import './table.css'
+import caretDown from '../assets/caret-down-fill.svg';
+import caretLeft from '../assets/caret-left.svg';
+import caretUp from '../assets/caret-up-fill.svg';
+import funnel from '../assets/funnel-fill.svg';
+import './table.css';
 
 
 class Table extends Component {
+    
     state = {
-        searchElement: null,
-        indexElement: '',
-        
-        searchFiltered: '',
-        indexFiltered: '',
+        inputField: null,
+        inputFieldIndex: '',
+
+        filterString: '',
+
+        sortDirection: 1,
+        indexSort: '',
+
+        data: this.props.data
     }
 
-    // Метод создание стрелки "направления фильтрации"
-    caret = (index) =>{
-        if (this.props.indexSort === index){
-            if (this.props.direction === -1){
-                return(<img src={caretUp} alt="Up"/>);
-            } else {
-                return(<img src={caretDown} alt="Down"/>);
-            }
-        } else {
-            return(<img src={caretLeft} alt="Left"/>);
-        }
-    }
+    //Сортировка данных
+    sortData = (index) => {
 
-    // Создание поля ввода
-    createInput = (index) => {
-        if (index === this.state.indexElement) {
+        const copyData = this.props.data.concat();
+
+        if (index === '#') {
             this.setState({
-                searchElement: null,
-                indexElement: '',
-                searchFiltered: ''
+                data: this.props.data,
+                sortDirection: 1,
+                indexSort: index
+            })
+            return;
+        }
+
+        if (index === this.state.indexSort) {
+            copyData.sort(
+                (a, b) => { return a[index] > b[index] ? this.state.sortDirection : -this.state.sortDirection}
+            )
+            this.setState({
+                data: copyData,
+                sortDirection: -this.state.sortDirection,
+                indexSort: index
             })
         } else {
+            copyData.sort(
+                (a, b) => { return a[index] > b[index] ? 1 : -1 }
+            )
             this.setState({
-                searchElement:
-                    <input
-                        type="text"
-                        className="form-control"
-                        onChange={this.onChange}
-                        onKeyPress={this.onKeyPress}
-                    />,
-                indexElement: index,
-                searchFiltered: ''
+                data: copyData,
+                sortDirection: -1,
+                indexSort: index
             })
         }
+
     }
 
-    // Методы отвечающие за заполнение полей: "текст по которому будет проведена фильтрация"
-    // и "индекс поля ввода по которому будет проведена фильтрация"
-    contentInput = ''; //
-    onChange = event => {
-        this.contentInput = event.target.value;
-    }
-
-    onKeyPress = (e) => {
-        if (e.key === 'Enter') {
-            this.setState({searchFiltered: this.contentInput});
-            this.setState({indexFiltered: this.state.indexElement});
-        }
-    }
-
-    // Вывод (показ) поля вода в таблице
-    viewInput = (index) => {
-        if (index === this.state.indexElement) {
-            return this.state.searchElement;
-        }
-    }
-
-    // Создание заполнения заголовка столбца
-    createColumnHeader  = (index, name) => {
-        return(
-            <div className="stick height">
-                <div className="nowrap pe-2" onClick={()=> {this.props.sortData(index)}}>
-                        {name}
-                        {this.caret(index)}
-                </div>
-                <div className="d-flex justify-content-end mt-1">
-                    {this.viewInput(index)}
-                    <button
-                        type="button"
-                        className="btn btn-light"
-                        onClick={() => this.createInput(index)}
-                    >
-                        <img src={funnel} alt="funnel"/>
-                    </button>
-                </div>
-            </div>
+    // Фильтрация данных    
+    filteredData = (inputFieldIndex, filterString) => {
+        return this.state.data.filter(
+            item => String(item[inputFieldIndex]).toLowerCase().includes(filterString.toLowerCase())
         )
     }
 
-    render() {
-        const finishData = this.props.filteredData(this.state.indexFiltered, this.state.searchFiltered)
+    // вывод шапки таблицы
+    getHead = (columns) => {
 
-        var i = 1;
-        return(
+        // Создание стрелки "направления фильтрации"
+        const showSortArrows = (index) => {
+            if (this.state.indexSort === index) {
+                if (this.state.sortDirection === -1) {
+                    return (<img src={caretUp} alt="Up" />);
+                } else {
+                    return (<img src={caretDown} alt="Down" />);
+                }
+            } else {
+                return (<img src={caretLeft} alt="Left" />);
+            }
+        }
+
+        // Создание поля ввода
+        const createInput = (index) => {
+            let contentInput = ""
+
+            if (index === this.state.inputFieldIndex) {
+                this.setState({
+                    inputField: null,
+                    inputFieldIndex: '',
+                    filterString: ''
+                })
+            } else {
+                this.setState({
+                    inputField:
+                        <input
+                            type="text"
+                            className="form-control"
+                            onChange={(event) => {
+                                contentInput = event.target.value;
+                            }}
+                            onKeyPress={(event) => {
+                                if (event.key === 'Enter') {
+                                    this.setState({ filterString: contentInput });
+                                }
+                            }}
+                        />,
+                    inputFieldIndex: index,
+                    filterString: ''
+                })
+            }
+        }
+
+        // Показ поля ввода в таблице
+        const showInput = (index) => {
+            if (index === this.state.inputFieldIndex) {
+                return this.state.inputField;
+            }
+        }
+
+        // Возвращение шапки таблицы
+        return (
+            <tr>
+
+                <th>
+                    <div className="stick height pe-2" onClick={() => { this.sortData('#'); }}>#</div>
+                </th>
+
+                {columns.map(item => (
+                    <th key={item.index}>
+                        <div className="stick height">
+                            <div className="nowrap pe-2" onClick={() => { this.sortData(item.index) }}>
+                                {item.name}
+                                {showSortArrows(item.index)}
+                            </div>
+                            <div className="d-flex justify-content-end mt-1">
+                                {showInput(item.index)}
+                                <button
+                                    type="button"
+                                    className="btn"
+                                    onClick={() => createInput(item.index)}
+                                >
+                                    <img src={funnel} alt="funnel" />
+                                </button>
+                            </div>
+                        </div>
+                    </th>
+                ))}
+
+            </tr>
+        )
+    }
+
+    // Вывод контента таблицы (НАДО ЗАМЕНИТЬ ВСЕ НАИМЕНОВАНИЯ)
+    getContent = (data, columnIndices) => {
+        let content = [];
+        let a = 1;
+        // let key = 0;
+
+        for (let i = 0; i < data.length; i++) {
+            let line = [<td className="fw-bold" key={"#" + i.toString()}>{++a}</td>];
+            // console.log("#" + i.toString())
+            const tableRow = data[i];
+            for (let j = 0; j < columnIndices.length; j++) {
+                const elementIndex = columnIndices[j];
+                line.push(<td key={elementIndex.toString() + i.toString()}>{tableRow[elementIndex]}</td>)
+                // console.log(elementIndex.toString() + i.toString())
+            }
+            content.push(<tr key={"line" + i.toString()}>{line}</tr>)
+            // console.log("line" + i.toString())
+        }
+
+        return content;
+    };
+
+    render() {
+
+        const finishData = this.filteredData(this.state.inputFieldIndex, this.state.filterString)
+
+        const columnIndices = [];
+        this.props.columns.map(item => (columnIndices.push(item.index)))
+
+        return (
             <div className="table-responsive mt-3">
                 <table className="table">
                     <thead className="table-light">
-                        <tr>
-                            <th>
-                                <div className="stick height pe-2" onClick={()=> {this.props.sortData('#');}}>#</div>
-                            </th>
-                            <th>
-                                {this.createColumnHeader('id', 'ID')}
-                            </th>
-                            <th>
-                                {this.createColumnHeader('surname', 'Surname')}
-                            </th>
-                            <th>
-                                {this.createColumnHeader('name', 'Name')}
-                            </th>
-                            <th>
-                                {this.createColumnHeader('patronymic', 'Patronymic')}
-                            </th>
-                            <th>
-                                {this.createColumnHeader('phone', 'Phone')}
-                            </th>
-                            <th>
-                                {this.createColumnHeader('date', 'Date')}
-                            </th>
-                        </tr>
+                        {this.getHead(this.props.columns)}
                     </thead>
                     <tbody>
-                        {finishData.map(item =>(
-                            <tr key={item.id}>
-                                <td className="fw-bold">{i++}</td>
-                                <td>{item.id}</td>
-                                <td>{item.surname}</td>
-                                <td>{item.name}</td>
-                                <td>{item.patronymic}</td>
-                                <td>{item.phone}</td>
-                                <td>{item.date}</td>
-                            </tr>
-                        ))}
+                        {this.getContent(finishData, columnIndices)}
                     </tbody>
                 </table>
             </div>
         )
+        
     }
 }
 
